9d2df7261143fa0ec1f966f990d086f99966802b
Danny <danny200073@gmail.com>
2024-06-13 17:28:12 +0800

    new

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './../app-routing.module';
 import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,6 +9,20 @@ import { RouterModule, Routes } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private http:HttpClient){}
   path:'posthome';
+  login(){
+    let body = {
+      "email": "test123@test.com",
+      "username": "test123",
+      "password": "test"
+    };
+
+    let url = "https://localhost:7148/api/LoginJWT/Log-in";
+
+    this.http.post<any>(url,body).subscribe(res=>{
+      console.log(res);
+    });
+  }
 
 }
